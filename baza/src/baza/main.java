1867ebf763eda144be65cdc23c9016b1099af3b1
Kamil_Jarmoc <kam498@wp.pl>
2018-06-18 18:07:33 +0200

    ?

--- baza/src/baza/main.java
+++ baza/src/baza/main.java
@@ -1,5 +1,29 @@
 package baza;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.clients.MongoClients;
+import com.mongodb.clients.MongoClient;
+
+////////
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI
+////////////
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
 public class main {
 
 	public static void main(String[] args) {
